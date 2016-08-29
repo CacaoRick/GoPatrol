cb13c3a3b8d54f01f2cca74ad8d1bcece319532b
CacaoRick <cacaorick@gmail.com>
2016-08-30 00:01:15 +0800

    加入管理員功能，完成 Group 機器人功能

--- example_config.js
+++ example_config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	/**
+	 * 帳號、密碼、帳號類型（google / ptc）
 	 * 用這個一定有被BAN的風險，請勿使用主帳號登入！
-	 * 帳號、密碼、帳號類型（google / ptc），可輸入多組
-	 * 範例：
+	 * 可輸入多組，格式參考以下範例：
 	 * account:[
 	 * 	{username: "你的帳號A", password: "帳號A的密碼", provider: "google"},
 	 * 	{username: "你的帳號B", password: "帳號B的密碼", provider: "google"},
@@ -14,37 +14,59 @@ module.exports = {
 	],
 
 	/**
-	 * 找 BotFather 建立機器人後可取得
+	 * 機器人 Token
+	 * 請用 Telegram 找 @BotFather 建立機器人後可取得
 	 * 範例：
 	 * telegramBotToken: "110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw",
 	 */
-    telegramBotToken: "改為你的token",
+	telegramBotToken: "改為你的token",
 
-    /**
-     * 機器人模式（適合個人使用），去掉引號填入 null
+	/**
+	 * 管理者名單
+	 * 只有管理者可以使用機器人修改設定（/start、/stop、傳送位置訊息更改中心）
+	 * 填入使用者名稱，可設定多個使用者為管理者（注意，這裡不須@）
+	 * telegramAdminUsername: ["AdminUser1", "AdminUser2"], 
+	 */
+	telegramAdminUsernames: ["UserName"],
+
+	/**
+     * 機器人模式（適合個人與群組使用），去掉引號填入 null
      * telegramChannelID: null,
      * 
-     * 廣播頻道模式，填入含@之群組或頻道ID
+     * 廣播頻道模式，填入含@之頻道ID
      * telegramChannelID: "@ChannelID",
      */
     telegramChannelID: "@你的ChannelID",
 
-    // 巡邏中心位置
-    initCenterLocation: {
+	/**
+	 * 巡邏中心位置
+	 * latitude: 緯度
+	 * longitude: 經度
+	 * 可利用 google map 等工具查詢想要的位置，網址中會有緯度與精度可以複製來用
+	 */
+	initCenterLocation: {
 		latitude: 25.0339031,
 		longitude: 121.5623212
 	},
 
-	// 搜尋範圍大約是這個數字*100公尺為半徑，設越大要找越久，可設定更多帳號來減少搜尋時間
+	/**
+	 * 搜尋範圍
+	 * 範圍大約是這個數字*100公尺為半徑，設越大要找越久
+	 * 可設定更多帳號來減少搜尋時間，但同IP用太多帳號會被 BAN IP
+	 */
 	searchSteps: 2,
 
-	// 搜尋延遲
+	/**
+	 * 搜尋延遲
+	 * 移動後停留在一個位置的時間，若 searchSteps 為 1 可以設為 0
+	 * searchSteps > 1 建議要設定 1000 豪秒以上，不然有些寶可夢還沒被發現你就跑走了
+	 */
 	searchDelay: 1000,
 
 	/**
-	 * 寶可夢黑名單，設定後不會通知，編號可參考 pokemon_name.js
-	 * 範例：
-	 * blacklist: [1, 2, 3, 4, 5, 65, 75, 102]
+	 * 寶可夢黑名單
+	 * 設定後不會通知，編號可參考 pokemon_name.js
+	 * 範例：blacklist: [10, 13, 16, 19]
 	 */
 	blacklist: []
 };
--- index.js
+++ index.js
@@ -25,24 +25,19 @@ class TelegramBotWithVenus extends TelegramBot {
 const event = new EventEmitter();
 var telegramBot = new TelegramBotWithVenus(config.telegramBotToken, { polling: true });
 var pokespotter = Pokespotter(config.account);
-pokespotter.DEBUG = false;
 
-var blacklist = config.blacklist;
-var centerLocation = config.initCenterLocation;
+var telegramAdminUsernames = config.telegramAdminUsernames;	// 管理員名單
+var blacklist = config.blacklist;	// 寶可夢黑名單
+var centerLocation = config.initCenterLocation;	// 搜尋中心位置
 var spotterOptional = {
-	steps: config.searchSteps,
-	requestDelay: config.searchDelay,
+	steps: config.searchSteps,			// 搜尋範圍
+	requestDelay: config.searchDelay,	// 搜尋延遲
 	currentTime: Date.now()
 }
 
-// 巡邏中
-var isPatrolling = false;
-
-// 儲存的寶可夢
-var pokemons = [];
-
-// 啟動中的Telegram使用者ID
-var activeChatIDs = [];
+var isPatrolling = false;	// 巡邏執行狀態
+var pokemons = [];			// 儲存的寶可夢
+var activeChatIDs = [];		// 啟動中的 Telegram ChatID
 
 // 巡邏
 event.on("patrol", function() {
@@ -115,13 +110,6 @@ event.on("checkLastTime", function() {
 	doNextPatrol();
 });
 
-event.on("informAllPokemons", function(chatId) {
-	for (var i = 0; i < pokemons.length; i++) {
-		var lastTime = getLastTime(pokemons[i].expirationTime);
-		sendPokemon(chatId, pokemons[i], lastTime);
-	}
-});
-
 // 將寶可夢通知給所有啟動中的使用者
 event.on("informToActiveUsers", function(pokemon, lastTime) {
 	for (var i = 0; i < activeChatIDs.length; i++) {
@@ -184,33 +172,33 @@ if (channelID != null) {
 	console.log("機器人模式啟動，請在 Telegram 聊天中傳送指令");
 	// Bot 收到訊息，處理指令
 	telegramBot.on("message", function(msg) {
-		var chatId = msg.from.id;
+		var chatId = msg.chat.id;
+		var isAdmin = telegramAdminUsernames.indexOf(msg.from.username) >= 0;
+
+		var command = msg.text.split("@")[0];
 
 		// 發送說明
-		if (msg.text == "/help") {
+		if (command == "/help") {
 			telegramBot.sendMessage(
 				chatId,
 				"說明：\n" +
 				"以指定的位置為中心進行巡邏（範圍約半徑100多公尺），將遇到的寶可夢通知給使用者\n" +
-				"傳送位置訊息可更改巡邏中心位置\n" +
-				"指令：\n" +
+				"一般指令：\n" +
+				"/getmap 取得附近寶可夢地圖\n\n" +
+				"管理員專用：\n" + 
 				"/help 查看說明\n" +
 				"/start 開始巡邏\n" +
 				"/stop 停止巡邏\n" +
-				"/getmap 取得附近寶可夢地圖\n"
+				"傳送位置訊息可更改巡邏中心位置"
 			);
 		}
 
-		// 登錄使用者，若巡邏為執行則觸發巡邏
-		if (msg.text == "/start") {
-			telegramBot.sendMessage(chatId, "開始接收通知");
-
+		// 登錄chatId，若巡邏為執行則觸發巡邏
+		if (command == "/start" && isAdmin) {
 			// 若 chatId 不在清單中，加進去
 			if (activeChatIDs.indexOf(chatId) < 0) {
 				activeChatIDs.push(chatId);
 				console.log("新增使用者：" + chatId + "，目前使用者：" + activeChatIDs);
-				// 對新使用者全部通知一次附近的寶可夢
-				event.emit("informAllPokemons", chatId);
 			}
 
 			// 若原本沒在執行中，觸發巡邏並更改執行狀態
@@ -219,49 +207,48 @@ if (channelID != null) {
 				event.emit("patrol");
 				// 更改執行狀態
 				isPatrolling = true;
+				telegramBot.sendMessage(chatId, "管理員啟動通知");
+			} else {
+				// 本來就在巡邏了
+				telegramBot.sendMessage(chatId, "通知已進行中");
 			}
 		}
 
-		// 從清單中移除使用者，使用者將不再被通知
-		if (msg.text == "/stop") {
+		// 從清單中移除chatId，該chatId將不再被通知
+		if (command == "/stop" && isAdmin) {
 			// 從 activeChatIDs 中移除
 			var index = activeChatIDs.indexOf(chatId);
 			if (index >= 0) {
 				activeChatIDs.splice(index, 1);
 			}
 
-			telegramBot.sendMessage(chatId, "已停止接收通知");
+			telegramBot.sendMessage(chatId, "管理員停止通知");
 		}
 
 		// 取得附近寶可夢的地圖圖檔
-		if (msg.text == "/getmap") {
+		if (command == "/getmap") {
 			event.emit("getmap", chatId);
 		}
 	});
 
 	telegramBot.on("location", function(msg) {
-		// 更改座標
-		centerLocation = msg.location;
-
-		// 準備通知訊息
-		var name = msg.from.username;
-		if (typeof name === "undefined") {
-			name = msg.from.id;
+		// 判斷是否為管理員
+		if (telegramAdminUsernames.indexOf(msg.from.username) >= 0) {
+			// 更改座標
+			centerLocation = msg.location;
+			// 通知
+			telegramBot.sendMessage(msg.chat.id, "已更改巡邏中心位置");
 		}
-		var message = name + " 更改巡邏中心位置";
-		console.log(message);
-
-		// 通知下指令者
-		telegramBot.sendMessage(msg.from.id, "已更改巡邏中心位置");
-
-		// 通知其他使用者
-		activeChatIDs.forEach(function(id) {
-			if (id != msg.from.id) {
-				telegramBot.sendVenue(id, centerLocation.latitude, centerLocation.longitude, message, centerLocation.latitude + ", " + centerLocation.longitude);
-			}
-		});
 	});
 }
+
+
+
+
+
+
+
+
 
 function sendPokemon(chatId, pokemon, lastTime) {
 	telegramBot.sendVenue(
@@ -269,7 +256,7 @@ function sendPokemon(chatId, pokemon, lastTime) {
 		pokemon.latitude,
 		pokemon.longitude,
 		"#" + pokemon.pokemonId + " " + pokemonNames[pokemon.pokemonId] + " " + pokemon.distance + "m",
-		"剩餘:" + getMMSS(lastTime) + " 結束:" + getHHMMSS(pokemon.expirationTime)
+		"剩餘 " + getMMSS(lastTime) + " 結束 " + getHHMMSS(pokemon.expirationTime)
 	);
 }
 
