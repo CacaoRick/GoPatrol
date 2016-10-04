7452668965c43a9150eca3b98cfc18aa936b88d8
CacaoRick <cacaorick@gmail.com>
2016-10-04 22:27:42 +0800

    將 telegramBot.js 拿掉 直接用 dimo 版的 node-telegram-bot-api。修正版本檢查版本號寫錯。

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 'use strict';
 const packagejson = require('./package.json');
 console.log(`Node v${process.versions.node} | GoPatrol v${packagejson.version}`);
-if (parseInt(process.versions.node.charAt(0)) < "7") {
+if (parseInt(process.versions.node.charAt(0)) < "6") {
 	console.log("請使用 Node v6.4.0 以上版本");
 	process.exit("1");
 }
 const config = require("./config.js");
-const TelegramBot = require("./telegramBot.js");
 const pokemonNames = require("./pokemon_names.js");
+
 const _ = require("lodash");
 const Jimp = require("jimp");
 const moment = require("moment");
 const request = require('request');
-const Pokespotter = require("pokespotter");
 const EventEmitter = require("events");
+const Pokespotter = require("pokespotter");
+const TelegramBot = require("node-telegram-bot-api");
 
 const event = new EventEmitter();
 const telegramBot = new TelegramBot(config.telegramBotToken, { polling: true });
