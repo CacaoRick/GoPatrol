edd63c8a6c5e4f55658ca1c55f72465b948d8bb8
CacaoRick <cacaorick@gmail.com>
2016-10-06 21:24:10 +0800

    將 process.exit 改為傳入-1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const packagejson = require('./package.json');
 console.log(`Node v${process.versions.node} | GoPatrol v${packagejson.version}`);
 if (parseInt(process.versions.node.charAt(0)) < "6") {
 	console.log("請使用 Node v6.4.0 以上版本");
-	process.exit("1");
+	process.exit("-1");
 }
 
 const fs = require("fs");
